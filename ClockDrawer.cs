bc3442c117863c66a888ccd39c37dc01e3cdb8a2
Jesse de Jonge <139691115+JesseDeJonge1@users.noreply.github.com>
2024-11-10 14:03:00 +0100

    C# Clock

---
+++ ClockDrawer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    /// <summary>
+    /// The ClockDrawer class is responsible for drawing a clock on a given Graphics object.
+    /// </summary>
+    public class ClockDrawer
+    {
+        /// <summary>
+        /// Draws the clock on the specified Graphics object.
+        /// </summary>
+        /// <param name="g">The Graphics object to draw on.</param>
+        /// <param name="width">The width of the drawing area.</param>
+        /// <param name="height">The height of the drawing area.</param>
+        public void DrawClock(Graphics g, int width, int height)
+        {
+            // Center and radius for the clock
+            int centerX = width / 2;
+            int centerY = height / 2;
+            int radius = Math.Min(centerX, centerY) - 10;
+
+            // Draw clock circle
+            g.DrawEllipse(Pens.Black, centerX - radius, centerY - radius, radius * 2, radius * 2);
+
+            // Get current time
+            DateTime now = DateTime.Now;
+            float secondAngle = now.Second * 6; // 360 / 60
+            float minuteAngle = now.Minute * 6 + now.Second * 0.1f; // 360 / 60
+            float hourAngle = (now.Hour % 12) * 30 + now.Minute * 0.5f; // 360 / 12
+
+            // Draw hands using trigonometry
+            DrawHand(g, centerX, centerY, secondAngle, radius - 20, Pens.Red); // Second hand
+            DrawHand(g, centerX, centerY, minuteAngle, radius - 30, Pens.Blue); // Minute hand
+            DrawHand(g, centerX, centerY, hourAngle, radius - 50, Pens.Black); // Hour hand
+        }
+
+        /// <summary>
+        /// Draws a clock hand on the specified Graphics object.
+        /// </summary>
+        /// <param name="g">The Graphics object to draw on.</param>
+        /// <param name="centerX">The x-coordinate of the clock center.</param>
+        /// <param name="centerY">The y-coordinate of the clock center.</param>
+        /// <param name="angle">The angle of the hand in degrees.</param>
+        /// <param name="length">The length of the hand.</param>
+        /// <param name="pen">The Pen object used to draw the hand.</param>
+        private void DrawHand(Graphics g, int centerX, int centerY, float angle, int length, Pen pen)
+        {
+            // Convert angle to radians and calculate the endpoint using sine and cosine
+            double radian = (Math.PI / 180) * angle;
+            int endX = centerX + (int)(length * Math.Sin(radian));
+            int endY = centerY - (int)(length * Math.Cos(radian));
+
+            // Draw line from center to endpoint
+            g.DrawLine(pen, centerX, centerY, endX, endY);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    /// <summary>
+    /// The Form1 class represents the main form of the clock application.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        private ClockDrawer clockDrawer;
+
+        /// <summary>
+        /// Initializes a new instance of the Form1 class.
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            clockDrawer = new ClockDrawer();
+
+            // Start the timer
+            timer1.Interval = 1000; // 1000 ms = 1 second
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Start();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the button1 control.
+        /// Forces the form to repaint.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">An EventArgs that contains the event data.</param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Force the form to repaint
+            this.Invalidate();
+        }
+
+        /// <summary>
+        /// Overrides the OnPaint method to draw the clock.
+        /// </summary>
+        /// <param name="e">A PaintEventArgs that contains the event data.</param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            clockDrawer.DrawClock(e.Graphics, this.ClientSize.Width, this.ClientSize.Height);
+        }
+
+        /// <summary>
+        /// Handles the Tick event of the timer1 control.
+        /// Forces the form to repaint every 1000 milliseconds.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">An EventArgs that contains the event data.</param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Force the form to repaint
+            this.Invalidate();
+        }
+    }
+}
